f1e82749939bcc4f2cde4ae0e48d1a353d292b40
Alejandro <aavila0707@gmail.com>
2023-03-12 11:42:28 +0100

    build: change content script location

--- background.js
+++ background.js
@@ -2,34 +2,3 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({ text: "OFF", });
   chrome.action.setBadgeBackgroundColor({ color: "#f44336" });
 });
-
-// async function getCurrentTab() {
-//   let queryOptions = { active: true, lastFocusedWindow: true };
-//   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-//   let [tab] = await chrome.tabs.query(queryOptions);
-//   return tab;
-// }
-
-// chrome.action.onClicked.addListener(async (tab) => {
-//   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-//   const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-//   await chrome.action.setBadgeText({
-//     tabId: tab.id,
-//     text: nextState,
-//   });
-
-//   if (nextState === "ON") {
-//     await chrome.tabs.sendMessage(tab.id, {
-//       message: "extension_on",
-//       tabId: tab.id,
-//     });
-//   }
-//   if (nextState === "OFF") {
-//     await chrome.tabs.sendMessage(tab.id, {
-//       message: "extension_off",
-//       tabId: tab.id,
-//     });
-//   }
-// });
-
