dc3590780becb6fb404ba3bbbe3c7dd58fadd478
Alejandro <aavila0707@gmail.com>
2023-03-12 12:43:14 +0100

    build: enable utf-8 only encoding for chrome standards in prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const webpack = require('webpack');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'extension'),
-    filename: 'content.js'
+    filename: 'content.js',
   },
   devtool: 'cheap-module-source-map',
   module: {
@@ -16,7 +18,16 @@ const config = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: { ascii_only: true },
+        },
+      }),
+    ],
+  },
 };
 
 module.exports = config;
